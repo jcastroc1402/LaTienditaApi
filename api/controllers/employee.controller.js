00c02c844b6063289615214fcf29f1c381583591
jcastroc1402 <jcastroc1402@gmail.com>
2020-07-22 03:28:29 -0500

    feat: Crud employee
    Crear primera version del crud de employee

--- api/controllers/employee.controller.js
+++ api/controllers/employee.controller.js
@@ -1,7 +1,56 @@
+import mapper from "automapper-js";
+import { EmployeeDto } from "../dtos";
+
 export class EmployeeController {
-  constructor() {}
+  constructor({ EmployeeService }) {
+    this._employeeService = EmployeeService;
+  }
 
   sayHello(req, res) {
     return res.send({ message: "Hola mundo empleado" });
+  }
+
+  async getEmployees(req, res) {
+    let employees = await this._employeeService.getAll();
+    employees = employees.map((employee) => mapper(EmployeeDto, employee));
+    return res.send({
+      payload: employees,
+    });
+  }
+
+  async getEmployee(req, res) {
+    const { id } = req.params;
+    let employee = await this._employeeService.get(id);
+    if (!employee) {
+      return res.status(404).send();
+    }
+    employee = mapper(EmployeeDto, employee);
+    return res.send({
+      payload: employee,
+    });
+  }
+
+  async createEmployee(req, res) {
+    const { body } = req;
+    const createdEmployee = await this._employeeService.create(body);
+    const employee = mapper(EmployeeDto, createdEmployee);
+    return res.status(201).send({
+      payload: employee,
+    });
+  }
+
+  async updateEmployee(req, res) {
+    const { body } = req;
+    const { id } = req.params;
+
+    await this._employeeService.update(id, body);
+    return res.status(204).send();
+  }
+
+  async deleteEmployee(req, res) {
+    const { id } = req.params;
+
+    await this._employeeService.delete(id);
+    return res.status(204).send();
   }
 }
--- api/routes/employee.routes.js
+++ api/routes/employee.routes.js
@@ -5,6 +5,17 @@ export default function ({ EmployeeController }) {
 
   //Rutas de empleado
   router.get("/hello", EmployeeController.sayHello);
+  router.get("/", EmployeeController.getEmployees.bind(EmployeeController));
+  router.get("/:id", EmployeeController.getEmployee.bind(EmployeeController));
+  router.post("/", EmployeeController.createEmployee.bind(EmployeeController));
+  router.put(
+    "/:id",
+    EmployeeController.updateEmployee.bind(EmployeeController)
+  );
+  router.delete(
+    "/:id",
+    EmployeeController.deleteEmployee.bind(EmployeeController)
+  );
 
   return router;
 }
